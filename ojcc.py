03f17c3fc1f710b8e7a2817c20065f2bf940805f
2h311 <kcodepal@outlook.com>
2023-03-24 16:56:34 -0500

    progress so far

--- ojcc.py
+++ ojcc.py
@@ -1,20 +1,33 @@
 import re
+import logging
 
 import requests
-from bs4 import BeautifulSoup
 from bs4.element import Tag
+from bs4 import BeautifulSoup
+
+
+# disable request logging messages
+logging.getLogger("requests").disabled = True
+logging.getLogger("urllib3").disabled = True
+
+
+logging.basicConfig(format="%(message)s")
+logger = logging.getLogger("ojcc")
+logger.setLevel(logging.DEBUG)
 
 
 search_text = "response to petition for benefits filed by"
-ojcc_case_no = "17-000001"
+ojcc_case_no = "17-000006"
 
 
-def get_jcc_html(ojcc_case_no: str):
+def get_jcc_html(ojcc_case_no: str) -> Tag:
     request_url = "https://www.jcc.state.fl.us/JCC/searchJCC/searchAction.asp?sT=byCase"
+    logger.debug(f"Visiting url -> {request_url}")
     response = requests.post(
-        request_url, data={"caseNum": ojcc_case_no, "Search": "+Search+"}
+        request_url, data={"caseNum": ojcc_case_no, "Search": "+Search+"}, timeout=45
     )
     response.raise_for_status()
+    logger.debug("Response OK. Cooking a beautiful soup")
     soup = BeautifulSoup(response.text, "html.parser")
     return soup.select_one("div#docket")
 
@@ -31,7 +44,9 @@ def get_pdf_links(div_docket: Tag) -> set:
         if proceedings_table_data.text.lower().__contains__(
             search_text
         ) and pdf_table_data.find("a"):
-            pdf_link = f'{"https://www.jcc.state.fl.us"}{pdf_table_data.find("a").get("href")}'
+            pdf_link = (
+                f'{"https://www.jcc.state.fl.us"}{pdf_table_data.find("a").get("href")}'
+            )
             pdf_link_list.add(pdf_link)
 
     if pdf_link_list:
@@ -42,4 +57,4 @@ def get_pdf_links(div_docket: Tag) -> set:
 
 div_docket = get_jcc_html(ojcc_case_no)
 if div_docket:
-	pdfs = get_pdf_links(div_docket)
+    reuslt = get_pdf_links(div_docket)
