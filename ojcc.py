92fa0c7e4b1e7e5ec438360c777d8a4f05235e08
2h311 <kcodepal@outlook.com>
2023-03-30 09:45:29 -0500

    progress from yesterday Mar 30, 2023

--- ojcc.py
+++ ojcc.py
@@ -1,9 +1,12 @@
 import re
+import io
 import logging
 
 import requests
 from bs4.element import Tag
 from bs4 import BeautifulSoup
+from pdfminer.high_level import extract_text
+from pdfminer.high_level import extract_pages
 
 
 # disable request logging messages
@@ -53,6 +56,7 @@ div_docket = get_jcc_html(ojcc_case_no)
 if div_docket:
     pdf_links = get_pdf_links(div_docket)
 
+
 pdf_link = pdf_links.pop()
 
 request_url = f"https://www.jcc.state.fl.us{pdf_link}"
@@ -62,17 +66,19 @@ response = requests.get(request_url, stream=True)
 response.raise_for_status()
 logger.debug("Download Successful. Reading content of PDF file")
 
-
-import io
-from pdfminer.high_level import extract_text
-from pdfminer.high_level import extract_pages
-
 text = extract_text(io.BytesIO(response.content))
 case_number = re.search("OJCC Case No.: (\S+)", text).groups()[0]
-print("Case Number:", case_number)
+logger.info(f"Case Number: {case_number}")
 
-telephone = re.search("\d{3}-\d{3}-\d{4}", text)
-email = re.search("([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+", text)
-# adjuster_name = ...
-#
-# text[:telephone.span()[0]]
+telephone = re.search("\d{3}-\d{3}-\d{4}", text).group()
+logger.info(f"Telephone: {telephone}")
+
+email = re.search(
+    "([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+", text
+).group()
+logger.info(f"Email: {email}")
+
+medical_benefits_case = re.search("MEDICAL BENEFITS CASE:\s+(\S+)", text).groups()[0]
+logger.info(f"MEDICAL BENEFITS CASE: {medical_benefits_case}")
+
+re.search("LOST TIME CASE:\s*", text)
