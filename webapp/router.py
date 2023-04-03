58e4de79cbd363a0d4329c14cf00b7453bf4f02a
2h311 <kcodepal@outlook.com>
2023-04-03 05:19:59 -0500

    added index.html and connected it to a get route

--- webapp/router.py
+++ webapp/router.py
@@ -1,10 +1,16 @@
 from fastapi import APIRouter
 from fastapi import Request
-
+from fastapi.templating import Jinja2Templates
 
 webapp_router = APIRouter(include_in_schema=False)
+templating = Jinja2Templates(directory="templates")
 
 
 @webapp_router.get("/")
 def index(request: Request):
-    return "index.html"
+    return templating.TemplateResponse("index.html", context={"request": request})
+
+
+@webapp_router.post("/")
+async def get_data(request: Request):
+    return "hello world - this is a post endpoint"
