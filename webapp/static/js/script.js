0e4358745c8cb6770bc9485269844def6f360f6b
2h311 <kcodepal@outlook.com>
2023-05-15 11:19:45 -0500

    picking up on this from where I left off

--- webapp/static/js/script.js
+++ webapp/static/js/script.js
@@ -43,10 +43,40 @@ searchButton.addEventListener("click", async function () {
   // make request to backend
   const data = await get_data_from_api(caseNumbers, caseStatusText);
 
+  const tableRow = document.createElement("tr");
+  tableRow.innerHTML = `
+    <thead>
+      <tr>
+        <th>Case &num;</th>
+        <th>Telephone</th>
+        <th>Email</th>
+        <th>Medical Benefits Case</th>
+        <th>Lost Time Case</th>
+        <th>PDF Link</th>
+      </tr>
+    </thead>
+  `;
+
   for (const item of data) {
+    const table = document.createElement("table");
     const caseNumber = item.userInputtedCaseNumber;
     console.log(caseNumber);
+    const caption = document.createElement("caption");
+    caption.innerText = caseNumber;
+    table.appendChild(caption);
+    table.appendChild(tableRow);
+    console.log(table);
+
     const cases = item.cases;
     console.log(cases);
+    item.cases.forEach((value, index, array) =>{
+      let caseNumber = value["caseNumber"];
+      let email = value.email;
+      let lostTimeCase = value.lostTimeCase;
+      let medicalBenefitsCase = value.medicalBenefitsCase;
+      let pdfLink = value.pdfLink;
+      let telephone = value.telephone;
+      console.log(email);
+    });
   }
 });
