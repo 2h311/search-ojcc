2f21976b35722b83b387a3e16da58d463aa05841
2h311 <kcodepal@outlook.com>
2023-05-23 16:04:57 -0500

    added te comma-separated ojcc case number functionality

--- webapp/static/js/script.js
+++ webapp/static/js/script.js
@@ -4,33 +4,39 @@ const tableResultsDiv = document.querySelector(".js-table-results");
 const loadingAnimationDiv = document.querySelector(".js-loading-animation");
 
 function getCaseNumbers() {
-  const caseNumbers = new Array();
-  document
-    .querySelectorAll("input[class|='js-case-no']")
-    .forEach((pageElement) => {
-      if (pageElement.value !== "") {
-        caseNumbers.push(pageElement.value);
-        pageElement.value = "";
-      }
-    });
-  return caseNumbers;
+  const cases = new Array();
+  const caseNumbers = document.querySelector(
+    "input[class|='js-case-no']"
+  ).value;
+  caseNumbers.split(",").forEach((string) => {
+    cases.push(string.trim());
+  });
+  return cases;
 }
 
 function getTableBody(cases) {
   let tbodyElement = "<tbody>";
   cases.forEach((value, index, array) => {
+    const {
+      caseNumber,
+      telephone,
+      email,
+      medicalBenefitsCase,
+      lostTimeCase,
+      pdfLink,
+    } = value;
     tbodyElement += `
-      <tr>
-        <td>${value.caseNumber}</td>
-        <td>${value.telephone}</td>
-        <td>${value.email}</td>
-        <td>${value.medicalBenefitsCase}</td>
-        <td>${value.lostTimeCase}</td>
-        <td>
-          <a href=${value.pdfLink}>${value.pdfLink}</a>
-        </td>
-      </tr>
-    `;
+	  <tr>
+		<td>${caseNumber}</td>
+		<td>${telephone}</td>
+		<td><a href="mailto:${email}"></a>${truncate(email)}</td>
+		<td>${medicalBenefitsCase}</td>
+		<td>${lostTimeCase}</td>
+		<td>
+			<a href=${pdfLink}>${pdfLink}</a>
+		</td>
+	  </tr>
+	`;
   });
   tbodyElement += "</tbody>";
   return tbodyElement;
@@ -41,22 +47,22 @@ function putApiDataonDOM(data) {
     const { userInputtedCaseNumber, cases } = item;
     let table;
     if (!cases.length) {
-      table = "<div>No response data for this case number</div>";
+      table = `<div>No response data for ${userInputtedCaseNumber}</div>`;
     } else {
       table = `<table>
-          <caption>${userInputtedCaseNumber}</caption>
-          <thead>
-            <tr>
-              <th>Case &num;</th>
-              <th>Telephone</th>
-              <th>Email</th>
-              <th>Medical Benefits Case</th>
-              <th>Lost Time Case</th>
-              <th>PDF Link</th>
-            </tr>
-          </thead>
-          ${getTableBody(cases)}
-      </table>`;
+		  <caption>${userInputtedCaseNumber}</caption>
+		  <thead>
+			<tr>
+			  <th>Case &num;</th>
+			  <th>Telephone</th>
+			  <th>Email</th>
+			  <th>Medical Benefits Case</th>
+			  <th>Lost Time Case</th>
+			  <th>PDF Link</th>
+			</tr>
+		  </thead>
+		  ${getTableBody(cases)}
+	  </table>`;
     }
     tableResultsDiv.innerHTML += table;
   }
@@ -75,19 +81,28 @@ async function getDataFromApi(caseNumbers, caseStatusText) {
   return await response.json();
 }
 
+function truncate(string, number = 29) {
+  let response = string;
+  if (string.length > number) {
+    response = string.slice(0, number - 1) + "&hellip;";
+  }
+  return response;
+}
+
 searchButton.addEventListener("click", async function () {
   const caseNumbers = getCaseNumbers();
+  // console.log(caseNumbers);
   if (!caseNumbers.length) {
     // TODO: display a red alert asking them to input something
     return;
   }
 
-  // // clear any existing table if any
-  // tableResultsDiv.innerHTML = "";
-  // // display a bouncing ball
-  // loadingAnimationDiv.innerHTML = `<img src="static/ball-animation.svg" alt="Loading Animation" class="loading-animation--ball">`;
+  // clear any existing table if any
+  tableResultsDiv.innerHTML = "";
+  // display a bouncing ball
+  loadingAnimationDiv.innerHTML = `<img src="static/ball-animation.svg" alt="Loading Animation" class="loading-animation--ball">`;
 
-  // const caseStatusText = caseStatusElement.selectedOptions[0].text;
-  // const data = await getDataFromApi(caseNumbers, caseStatusText); // make request to backend
-  // putApiDataonDOM(data);
+  const caseStatusText = caseStatusElement.selectedOptions[0].text;
+  const data = await getDataFromApi(caseNumbers, caseStatusText); // make request to backend
+  putApiDataonDOM(data);
 });
